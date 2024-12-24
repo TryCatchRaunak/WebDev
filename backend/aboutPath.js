4b3ee3dd81f5525fde1b5fe3895a891e913c4a1e
Raunak Mishra <69429073+TryCatchRaunak@users.noreply.github.com>
2024-12-24 23:42:09 +0530

    Learnt about path function in nodeJS

---
+++ backend/aboutPath.js
@@ -0,0 +1,9 @@
+import path from "path"
+
+let myPath = "C:\\Users\\rauna\\OneDrive\\Documents\\LEARNING\\javascript\\LearnJSwithME\\Asynchronous Javascript\\iife.js"
+console.log(path.extname(myPath))
+
+console.log(path.dirname(myPath))
+console.log(path.basename(myPath))
+
+console.log(path.join("c:/", "programs\\sample\\raunak.txt"))
