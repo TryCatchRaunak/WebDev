4e1affd8fa53175389ca57a050aeaa6051b50f2f
Raunak Mishra <69429073+TryCatchRaunak@users.noreply.github.com>
2024-12-25 20:09:51 +0530

    Learnt about page navigation, slug variable (requests and parameters) and usage of static files to make public using a 'public' folder

--- backend/http.js
+++ backend/http.js
@@ -26,6 +26,40 @@ app.get('/', (req, res) => {
     res.send('Hello World! My name is Raunak Mishra')
 })
 
+app.get('/about', (req, res) => {
+    res.send('About Page')
+})
+
+app.get('/contact', (req, res) => {
+    res.send('Contact Page')
+})
+
+app.get('/blog', (req, res) => {
+    res.send('Blog!')
+})
+
+// app.get('/blog/intro-to-javascript', (req, res) => {
+//     res.send('Introduction to Javacript')
+// })
+
+// app.get('/blog/intro-to-python', (req, res) => {
+//     res.send('Introduction to Python')
+// })
+
+// app.get('/blog/intro-to-java', (req, res) => {
+//     res.send('Introduction to Java')
+// })
+//But this type of code is not maintainable and scalable and this is one of the other reasons we use ExpressJS
+
+app.get('/blog/:slug', (req, res) => {
+    //logic to fetch {slug} from database
+    console.log(req.params)//prints { slug: 'intro-to-python' }
+    console.log(req.query)//prints { mode: 'dark', region: 'in' }
+    res.send(`Blog Post: ${req.params.slug}`)
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+app.use(express.static('public'))//making static files public to be accessed by anyone but this should not be done with main backend code bcz it can be accessed by anyone.
