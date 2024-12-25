4827433c5b0b3f89adb7dcf85bc644704323be53
Raunak Mishra <69429073+TryCatchRaunak@users.noreply.github.com>
2024-12-25 21:04:40 +0530

    Tested Put, Delete requests too using HTML

--- backend/requests.js
+++ backend/requests.js
@@ -15,6 +15,16 @@ app.post('/', (req, res) => {
     res.send('Hello World Post!')
 })
 
+app.put('/', (req, res) => {
+    console.log("Hey it is a put request")
+    res.send('Hello World Put!')
+})
+
+app.delete('/', (req, res) => {
+    console.log("Hey it is a delete request")
+    res.send('Hello World Delete!')
+})
+
 app.listen(port, () =>{
     console.log(`Server listening at http://localhost:${port}`)
 })
