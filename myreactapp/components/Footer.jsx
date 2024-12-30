b097a01f8c1eda9e9174a1aa929fe1574284b1f8
Raunak Mishra <69429073+TryCatchRaunak@users.noreply.github.com>
2024-12-30 22:02:07 +0530

    saw through props for react and also made some other components

---
+++ myreactapp/components/Footer.jsx
@@ -0,0 +1,11 @@
+import 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <h1>I am a FOOTER</h1>
+    </div>
+  )
+}
+
+export default Footer
--- myreactapp/components/Navbar.jsx
+++ myreactapp/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import 'react'
 
-const Navbar = () => {
+// const Navbar = ({logoText}) => {//This is a way
+
+const Navbar = (props) => {//This is another way of including props into jsx
   return (
     <div>
+      <div className="logo">{props.logoText}</div>
       <ul>
         <li>Home</li>
         <li>About</li>
