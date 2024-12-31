b5bc5c818e844ba844905edb3afa011dece90154
Raunak Mishra <69429073+TryCatchRaunak@users.noreply.github.com>
2024-12-31 12:00:19 +0530

    Learnt how to use vite to setup react app and also tried to make a basic webpage

--- myreactapp/components/Navbar.jsx
+++ myreactapp/components/Navbar.jsx
@@ -1,17 +1,18 @@
 import 'react'
 
 // const Navbar = ({logoText}) => {//This is a way
-
 const Navbar = (props) => {//This is another way of including props into jsx
   return (
-    <div>
-      <div className="logo">{props.logoText}</div>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact Us</li>
-      </ul>
-    </div>
+    <>
+      <div>
+        <div className="logo">{props.logoText}</div>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+    </>
   )
 }
 
