03dec3a508ac4bcadba9fc8509be409f667e35b7
Raunak Mishra <69429073+TryCatchRaunak@users.noreply.github.com>
2025-03-02 18:33:14 +0530

    Basics of React-Routing

---
+++ react-router/src/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>I am HOME</div>
+  )
+}
+
+export default Home
---
+++ react-router/src/components/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>I am LOGIN</div>
+  )
+}
+
+export default Login
